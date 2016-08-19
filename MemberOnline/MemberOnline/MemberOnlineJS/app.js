3cdfa71a5201c3fc677f3c539023de5f06bba400
umapornPoy <p.tunlang7@gmail.com>
2016-08-19 16:51:02 +0700

    set ng-route to member project

---
+++ MemberOnline/MemberOnline/MemberOnlineJS/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('MemberOnline', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "Home/Index"
+        })
+        .otherwise({
+            templateUrl: "Home/Index"
+        })
+});
