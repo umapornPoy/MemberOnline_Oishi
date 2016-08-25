cd55fd10267b089c5fe227f6161e7a8022146c04
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-08-25 15:53:43 +0700

    edit register

--- MemberOnline/MemberOnline/MemberOnlineJS/picture.js
+++ MemberOnline/MemberOnline/MemberOnlineJS/picture.js
@@ -1,1 +1,14 @@
-﻿angular.module('myApplication', ['camera']);
+﻿     function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result)
+                        .width(150)
+                        .height(150);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
