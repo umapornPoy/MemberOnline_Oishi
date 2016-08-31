1c65d2982bf11306384a9cf81910d0b431549205
umapornPoy <p.tunlang7@gmail.com>
2016-08-31 13:23:31 +0700

    update card page

--- MemberOnline/MemberOnline/MemberOnlineJS/js.js
+++ MemberOnline/MemberOnline/MemberOnlineJS/js.js
@@ -1,86 +1,70 @@
-﻿$(document).ready(function () {
-    // DOM ready
 
-    // Test data
-    /*
-     * To test the script you should discomment the function
-     * testLocalStorageData and refresh the page. The function
-     * will load some test data and the loadProfile
-     * will do the changes in the UI
-     */
-    // testLocalStorageData();
-    // Load profile if it exits
-    loadProfile();
+$('.input-cart-number').on('keyup change', function(){
+  $t = $(this);
+  
+  if ($t.val().length > 3) {
+    $t.next().focus();
+  }
+  
+  var card_number = '';
+  $('.input-cart-number').each(function(){
+    card_number += $(this).val() + ' ';
+    if ($(this).val().length == 4) {
+      $(this).next().focus();
+    }
+  })
+  
+  $('.credit-card-box .number').html(card_number);
 });
 
-/**
- * Function that gets the data of the profile in case
- * thar it has already saved in localstorage. Only the
- * UI will be update in case that all data is available
- *
- * A not existing key in localstorage return null
- *
- */
-function getLocalProfile(callback) {
-    var profileImgSrc = localStorage.getItem("PROFILE_IMG_SRC");
-    var profileName = localStorage.getItem("PROFILE_NAME");
-    var profileReAuthEmail = localStorage.getItem("PROFILE_REAUTH_EMAIL");
-    var profileReAuthEmail = localStorage.getItem("PROFILE_REAUTH_NUMBER");
+$('#card-holder').on('keyup change', function(){
+  $t = $(this);
+  $('.credit-card-box .card-holder div').html($t.val());
+});
 
-    if (profileName !== null
-            && profileReAuthEmail !== null
-            && profileReAuthNumber !== null
-            && profileImgSrc !== null) {
-        callback(profileImgSrc, profileName, profileReAuthEmail, profileReAuthNumber);
-    }
+$('#card-holder').on('keyup change', function(){
+  $t = $(this);
+  $('.credit-card-box .card-holder div').html($t.val());
+});
+
+$('#card-expiration-month, #card-expiration-year').change(function(){
+  m = $('#card-expiration-month option').index($('#card-expiration-month option:selected'));
+  m = (m < 10) ? '0' + m : m;
+  y = $('#card-expiration-year').val().substr(2,2);
+  $('.card-expiration-date div').html(m + '/' + y);
+})
+
+$('#card-ccv').on('focus', function(){
+  $('.credit-card-box').addClass('hover');
+}).on('blur', function(){
+  $('.credit-card-box').removeClass('hover');
+}).on('keyup change', function(){
+  $('.ccv div').html($(this).val());
+});
+
+
+/*--------------------
+CodePen Tile Preview
+--------------------*/
+setTimeout(function(){
+  $('#card-ccv').focus().delay(1000).queue(function(){
+    $(this).blur().dequeue();
+  });
+}, 500);
+
+/*function getCreditCardType(accountNumber) {
+  if (/^5[1-5]/.test(accountNumber)) {
+    result = 'mastercard';
+  } else if (/^4/.test(accountNumber)) {
+    result = 'visa';
+  } else if ( /^(5018|5020|5038|6304|6759|676[1-3])/.test(accountNumber)) {
+    result = 'maestro';
+  } else {
+    result = 'unknown'
+  }
+  return result;
 }
 
-/**
- * Main function that load the profile if exists
- * in localstorage
- */
-function loadProfile() {
-    if (!supportsHTML5Storage()) { return false; }
-    // we have to provide to the callback the basic
-    // information to set the profile
-    getLocalProfile(function (profileImgSrc, profileName, profileReAuthEmail,profileReAuthNumber) {
-        //changes in the UI
-        $("#profile-img").attr("src", profileImgSrc);
-        $("#profile-name").html(profileName);
-        $("#reauth-email").html(profileReAuthEmail);
-        $("#inputEmail").hide();
-        $("#reauth-number").html(profileReAuthNumber);
-        $("#inputNumber").hide();
-        $("#remember").hide();
-    });
-}
-
-/**
- * function that checks if the browser supports HTML5
- * local storage
- *
- * @returns {boolean}
- */
-function supportsHTML5Storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
-    }
-}
-
-/**
- * Test data. This data will be safe by the web app
- * in the first successful login of a auth user.
- * To Test the scripts, delete the localstorage data
- * and comment this call.
- *
- * @returns {boolean}
- */
-function testLocalStorageData() {
-    if (!supportsHTML5Storage()) { return false; }
-    localStorage.setItem("PROFILE_IMG_SRC", "//lh3.googleusercontent.com/-6V8xOA6M7BA/AAAAAAAAAAI/AAAAAAAAAAA/rzlHcD0KYwo/photo.jpg?sz=120");
-    localStorage.setItem("PROFILE_NAME", "César Izquierdo Tello");
-    localStorage.setItem("PROFILE_REAUTH_NUMBER", "1234567890");
-    localStorage.setItem("PROFILE_REAUTH_EMAIL", "oneaccount@gmail.com");
-}
+$('#card-number').change(function(){
+  console.log(getCreditCardType($(this).val()));
+})*/
