15ece4af79dba068221c1ce3dd1534963d04ae30
RATTANAPORN SOMNUK <5630213004@email.psu.ac.th>
2016-09-05 12:44:16 +0700

    edit date in register.cshtml

--- MemberOnline/MemberOnline/MemberOnlineJS/app.js
+++ MemberOnline/MemberOnline/MemberOnlineJS/app.js
@@ -1,4 +1,4 @@
-﻿var app = angular.module('MemberOnline', ['ngRoute']);
+﻿var app = angular.module('MemberOnline', ['ngRoute', 'ui.bootstrap']);
 
 app.config(function ($routeProvider) {
     $routeProvider
---
+++ MemberOnline/MemberOnline/MemberOnlineJS/register.js
@@ -0,0 +1,44 @@
+﻿app.controller('RegisterController', ['$scope', function ($scope) {
+
+    // ------------------------------------ JavaScript -------------------------------- //
+
+    $scope.start = new Date();
+    //$scope.end = new Date();
+
+    //$scope.minStartDate = new Date(); //fixed date
+    //$scope.maxStartDate = $scope.end; //init value
+    //$scope.minEndDate = $scope.start; //init value
+    //$scope.maxEndDate = $scope.end; //fixed date same as $scope.maxStartDate init value
+
+    //$scope.$watch('start', function (v) {
+    //    $scope.minEndDate = v;
+    //});
+    //$scope.$watch('end', function (v) {
+    //    $scope.maxStartDate = v;
+    //});
+
+    $scope.format = "dd/MM/yyyy";
+    $scope.altInputFormats = ['d!/M!/yyyy'];
+    $scope.dateOptions = {
+        startingDay: 0,
+        showWeeks: false
+    };
+
+    $scope.popupStart = {
+        opened: false
+    };
+
+    $scope.openStart = function () {
+        $scope.popupStart.opened = true;
+    };
+
+    $scope.popupEnd = {
+        opened: false
+    };
+
+    $scope.openEnd = function () {
+        $scope.popupEnd.opened = true;
+    };
+
+    //----------------------------------------------------------------------------------------------//
+}])
